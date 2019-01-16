eb58e219825a2accdb61d21b936e93ad09b374b3
Steven Tran <steven.tran15@hotmail.com>
2019-01-16 11:07:38 -0500

    1

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const data = require('./data-service.js');
+const employeesJSON = require('./data/employees.json');
+const departmentsJSON = require('./data/departments.json');
+
+const http_port = process.env.PORT || 8080;
+
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(`${__dirname}/views/home.html`));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(`${__dirname}/views/about.html`));
+});
+
+app.get("/employees", (req, res) => {
+    res.json(employeesJSON);
+});
+
+app.get("/managers", (req, res) => {
+
+   //var json = employeesJSON;
+    let final;
+    for (let i = 0; i < employeesJSON.length; i++) {
+        if (employeesJSON[i].isManager === true) {
+           
+           
+           final += JSON.stringify(employeesJSON[i]);
+        }
+    }
+    //res.setHeader('Content-Type', 'application/json'); 
+    res.json(final);
+});
+
+app.get("/departments", (req, res) => {
+    res.json(departmentsJSON);
+});
+
+
+
+
+
+
+
+
+
+app.listen(http_port, function () {
+    console.log(`Express http server listening on port ${http_port}....`);
+});
