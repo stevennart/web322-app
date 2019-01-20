323a202445b9eed7fec140ed8a8dcfea2c535ce2
Steven Tran <steven.tran15@hotmail.com>
2019-01-20 17:41:36 -0500

    done

---
+++ data-service.js
@@ -0,0 +1,103 @@
+/*********************************************************************************
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Steven Tran Student ID: 105629174 Date: Jan 15th, 2019
+*
+* Online (Heroku) Link: 
+*
+********************************************************************************/ 
+
+const fs = require('fs'); // require fs module to work with file reading. 
+
+var employees = []; // used to send the array of employees objects to the client 
+var departments = []; // used to send the array of departments objects to the client 
+
+module.exports.initialize = function () {
+
+    fs.readFile('./data/employees.json', (err, data) => {
+
+        result = JSON.parse(data);
+
+        for (let i = 0; i < result.length; i++) {
+            employees.push(result[i]);
+        }
+
+    });
+
+    fs.readFile('./data/departments.json', (err, data) => {
+
+        result = JSON.parse(data); // parses the read file content and stores it into an array, and then loop through array to push into the departments array created above. 
+
+        for (let i = 0; i < result.length; i++) {
+            departments.push(result[i]);
+        }
+
+    });
+
+    return new Promise((resolve, reject) => {
+
+        resolve("success");
+        reject("unable to read file");
+    });
+
+
+};
+
+module.exports.getAllEmployees = function () {
+
+    return new Promise((resolve, reject) => {
+
+        if (employees.length === 0) {
+            reject("no results returned");
+        } else {
+            resolve(employees);
+        }
+
+    });
+
+
+
+};
+
+module.exports.getManagers = function () {
+
+    var managers = []; // used to send the array of manager objects to the client 
+
+    for (let i = 0; i < employees.length; i++) {
+        if (employees[i].isManager) {
+
+            managers.push(employees[i]);
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+
+        if (managers.length === 0) {
+            reject("no results returned");
+        } else {
+            resolve(managers);
+        }
+    });
+
+
+};
+
+module.exports.getDepartments = function () {
+
+
+    return new Promise((resolve, reject) => {
+
+        if (departments.length === 0) {
+            reject("no results returned");
+        } else {
+            resolve(departments);
+        }
+
+    });
+
+
+
+};
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+/*********************************************************************************
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Steven Tran Student ID: 105629174 Date: Jan 15th, 2019
+*
+* Online (Heroku) Link: 
+*
+********************************************************************************/ 
+
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const data = require('./data-service.js');
-const employeesJSON = require('./data/employees.json');
-const departmentsJSON = require('./data/departments.json');
-
+//const employeesJSON = require('./data/employees.json'); no longer needed because data-service module took care of reading the file contents and putting it into an array of objects. 
+//const departmentsJSON = require('./data/departments.json'); no longer needed because data-service module took care of reading the file contents and putting it into an array of objects. 
+ 
 const http_port = process.env.PORT || 8080;
-
 
 app.use(express.static('public'));
 
@@ -19,35 +31,59 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/employees", (req, res) => {
-    res.json(employeesJSON);
+
+    data.getAllEmployees().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({message: err});
+    });
+
 });
 
 app.get("/managers", (req, res) => {
 
 
-    var final = [];
-    for (let i = 0; i < employeesJSON.length; i++) {
-        if (employeesJSON[i].isManager) {
+    data.getManagers().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({message: err});
+    });
 
-            final.push(employeesJSON[i]);
-        }
-    }
-
-    res.json(final);
 });
 
 app.get("/departments", (req, res) => {
-    res.json(departmentsJSON);
+    
+    data.getDepartments().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({message: err});
+    });
+    
+});
+
+app.use((req, res) => { // if route doesn't match anything above, do this.
+
+    res.status(404).send(`
+    
+    <div style='display: flex; justify-content: center; align-items: center; height: 100%;'>
+    
+    <img src='https://cdn-images-1.medium.com/max/640/1*PayLNtfwPr4hIicJtwvVLA.png' alt='404img'>
+    
+    </div>
+     
+    
+    `);
+
 });
 
 
+data.initialize().then((data) => {
 
+    app.listen(http_port, function () {
+        console.log(data);
+        console.log(`Express http server listening on port ${http_port}....`);
+    });
 
-
-
-
-
-
-app.listen(http_port, function () {
-    console.log(`Express http server listening on port ${http_port}....`);
+}).catch(() => {
+    console.log(`No data was fetched, server failed to start up.`);
 });
