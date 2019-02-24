c9872ea8ed65fa207e84c0070c169f70b7ce3091
Steven Tran <steventran1534@gmail.com>
2019-02-24 13:16:05 -0500

    changed module.exports and using foreach

--- data-service.js
+++ data-service.js
@@ -10,233 +10,192 @@
  *
  ********************************************************************************/
 
-const fs = require('fs'); // require fs module to work with file reading. 
+const fs = require("fs"); // require fs module to work with file reading.
 
-let employees = []; // used to send the array of employees objects to the client 
+let employees = []; // used to send the array of employees objects to the client
 
-let departments = []; // used to send the array of departments objects to the client 
+let departments = []; // used to send the array of departments objects to the client
 
-module.exports.initialize = function () { // done
-
-    return new Promise((resolve, reject) => {
-
-        fs.readFile('./data/employees.json', (err, data) => {
-
-            if (err) {
-
-                reject("unable to read file");
-            }
-
-            result = JSON.parse(data);
-
-            for (let i = 0; i < result.length; i++) {
-                employees.push(result[i]);
-            }
-
-        });
-
-        fs.readFile('./data/departments.json', (err, data) => {
-
-            if (err) {
-
-                reject("unable to read file");
-            }
-
-            result = JSON.parse(data); // parses the read file content and stores it into an array, and then loop through array to push into the departments array created above. 
-
-            for (let i = 0; i < result.length; i++) {
-                departments.push(result[i]);
-            }
-
-        });
-
-        resolve("success");
-
-    });
-
-
-};
-
-module.exports.getAllEmployees = function () { // done
+module.exports = {
+  initialize: function() {
+    // done
 
     return new Promise((resolve, reject) => {
-
-        if (employees.length === 0) {
-
-            reject("no results returned");
-        } else {
-
-            resolve(employees);
+      fs.readFile("./data/employees.json", (err, data) => {
+        if (err) {
+          reject("unable to read file");
         }
 
+        result = JSON.parse(data);
+
+        for (let i = 0; i < result.length; i++) {
+          employees.push(result[i]);
+        }
+      });
+
+      fs.readFile("./data/departments.json", (err, data) => {
+        if (err) {
+          reject("unable to read file");
+        }
+
+        result = JSON.parse(data); // parses the read file content and stores it into an array, and then loop through array to push into the departments array created above.
+
+        for (let i = 0; i < result.length; i++) {
+          departments.push(result[i]);
+        }
+      });
+
+      resolve("success");
     });
+  },
 
-};
-
-module.exports.addEmployee = function (employeeData) { // done
+  getAllEmployees: function() {
+    // done
 
     return new Promise((resolve, reject) => {
-
-        if (!employeeData.isManager) {
-            employeeData.isManager = false;
-        } else {
-            employeeData.isManager = true;
-        }
-
-        employeeData.employeeNum = (employees.length + 1);
-
-        employees.push(employeeData);
-
-        resolve("employee added");
-
-        if (employees.length === 0) {
-
-            reject("no results returned");
-        }
-
+      if (employees.length === 0) {
+        reject("no results returned");
+      } else {
+        resolve(employees);
+      }
     });
+  },
 
-
-};
-
-module.exports.getEmployeesByStatus = function (status) { // done
+  addEmployee: function(employeeData) {
+    // done
 
     return new Promise((resolve, reject) => {
+      if (!employeeData.isManager) {
+        employeeData.isManager = false;
+      } else {
+        employeeData.isManager = true;
+      }
 
-        let employeeStatus = []; // a filter of all employees that has a pt/ft status
+      employeeData.employeeNum = employees.length + 1;
 
-        for (let i = 0; i < employees.length; i++) {
+      employees.push(employeeData);
 
-            if (employees[i].status == status) {
+      resolve("employee added");
 
-                employeeStatus.push(employees[i]);
-            }
-        }
-
-        if (employeeStatus.length === 0) {
-
-            reject("no results returned");
-        }
-
-        resolve(employeeStatus);
-
+      if (employees.length === 0) {
+        reject("no results returned");
+      }
     });
-};
+  },
 
-
-module.exports.getEmployeeByNum = function (num) { // done
+  getEmployeesByStatus: function(status) {
+    // done
 
     return new Promise((resolve, reject) => {
+      let employeeStatus = []; // a filter of all employees that has a pt/ft status
 
-        let empByNum = []; // filters a single employee's employeeNum that matches the num 
-
-        for (let i = 0; i < employees.length; i++) {
-
-            if (employees[i].employeeNum == num) {
-
-                empByNum = employees[i];
-            }
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].status == status) {
+          employeeStatus.push(employees[i]);
         }
+      }
 
+      if (employeeStatus.length === 0) {
+        reject("no results returned");
+      }
 
-        if (empByNum.length === 0) {
-
-            reject("no results returned");
-        }
-
-        resolve(empByNum);
-
+      resolve(employeeStatus);
     });
-};
+  },
 
-
-
-module.exports.getManagers = function () {
+  getEmployeeByNum: function(num) {
+    // done
 
     return new Promise((resolve, reject) => {
+      let empByNum = []; // filters a single employee's employeeNum that matches the num
 
-        var managers = []; // used to send the array of manager objects to the client 
-
-        for (let i = 0; i < employees.length; i++) {
-            if (employees[i].isManager) {
-
-                managers.push(employees[i]);
-            }
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].employeeNum == num) {
+          empByNum = employees[i];
         }
+      }
 
-        if (managers.length === 0) {
+      if (empByNum.length === 0) {
+        reject("no results returned");
+      }
 
-            reject("no results returned");
-        } else {
-
-            resolve(managers);
-        }
-
+      resolve(empByNum);
     });
-};
+  },
 
-module.exports.getEmployeesByManager = function (manager) { // done
+  getManagers: function() {
+    return new Promise((resolve, reject) => {
+      var managers = []; // used to send the array of manager objects to the client
+
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].isManager) {
+          managers.push(employees[i]);
+        }
+      }
+
+      if (managers.length === 0) {
+        reject("no results returned");
+      } else {
+        resolve(managers);
+      }
+    });
+  },
+
+  getEmployeesByManager: function(manager) {
+    // done
 
     return new Promise((resolve, reject) => {
+      var empByManager = []; // a filter of all employees's managerNum that matches the manager #
 
-        var empByManager = []; // a filter of all employees's managerNum that matches the manager #
-
-        for (let i = 0; i < employees.length; i++) {
-
-            if (employees[i].employeeManagerNum == manager) {
-
-                empByManager.push(employees[i]);
-            }
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].employeeManagerNum == manager) {
+          empByManager.push(employees[i]);
         }
+      }
 
-        if (empByManager.length == 0) {
+      if (empByManager.length == 0) {
+        reject("no results returned");
+      }
 
-            reject("no results returned");
-        }
-
-        resolve(empByManager);
-
+      resolve(empByManager);
     });
-};
+  },
 
+  getDepartments: function() {
+    return new Promise((resolve, reject) => {
+      if (departments.length === 0) {
+        reject("no results returned");
+      } else {
+        resolve(departments);
+      }
+    });
+  },
 
-
-module.exports.getDepartments = function () {
+  getEmployeesByDepartment: function(department) {
+    // done
 
     return new Promise((resolve, reject) => {
+      let departmentNum = []; // a filter of all employes that has a matching department id
 
-        if (departments.length === 0) {
-
-            reject("no results returned");
-        } else {
-
-            resolve(departments);
+      employees.forEach((element, index) => {
+        if (element.department == department) {
+          departmentNum.push(element);
         }
+      });
 
+      // for (let i = 0; i < employees.length; i++) {
+
+      //     if (employees[i].department == department) {
+
+      //         departmentNum.push(employees[i]);
+      //     }
+      // }
+
+      if (departmentNum.length == 0) {
+        reject("no results returned");
+      }
+
+      resolve(departmentNum);
     });
-};
-
-
-module.exports.getEmployeesByDepartment = function (department) { // done
-
-    return new Promise((resolve, reject) => {
-
-        let departmentNum = []; // a filter of all employes that has a matching department id
-
-        for (let i = 0; i < employees.length; i++) {
-
-            if (employees[i].department == department) {
-
-                departmentNum.push(employees[i]);
-            }
-        }
-
-        if (departmentNum.length == 0) {
-
-            reject("no results returned");
-        }
-
-        resolve(departmentNum);
-
-    });
+  }
 };
